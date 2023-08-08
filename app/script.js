4523917c57758a5f942633058b0ecf73c055ab58
ssamko0911 <ssamko091184@gmail.com>
2023-08-08 23:50:00 +0100

    Dark Mode implemented.

--- app/script.js
+++ app/script.js
@@ -1,4 +1,18 @@
+const imageDark = 'icons/mode-dark.png';
+const imageLight = 'icons/mode-white.png';
+
 const toggleMode = function () {
     const bodyElement = document.body;
+    const imageElement = document.querySelector('#btn-img');
+    const buttonElement = document.querySelector('#btn-mode')
+
     bodyElement.classList.toggle('dark-mode');
+
+    if (bodyElement.className === 'dark-mode') {
+        imageElement.src = imageLight;
+        imageElement.title = 'Light mode';
+    } else {
+        imageElement.src = imageDark;
+        imageElement.title = 'Dark mode';
+    }
 }
